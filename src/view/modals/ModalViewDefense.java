6feed9a418a0b3afe46a33926f63773ac0583a00
nbo2001 <natanaelbezerra36@gmail.com>
2023-06-02 09:31:46 -0400

    Defenses edit

--- src/view/modals/ModalViewDefense.java
+++ src/view/modals/ModalViewDefense.java
@@ -1,8 +1,7 @@
 package view.modals;
 
-import interfaces.VisiblePersonified;
 import interfaces.Visibled;
-import model.Defense;
+import model.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -28,63 +27,102 @@ public class ModalViewDefense implements Visibled {
         JPanel jPanel = new JPanel();
         jPanel.setLayout(new BoxLayout(jPanel,BoxLayout.Y_AXIS));
 
-        JPanel topView = new JPanel();
-        topView.setLayout(new BoxLayout(topView,BoxLayout.Y_AXIS));
-
-        JLabel jLabel = new JLabel("Informações");
-        JPanel title = new JPanel();
-        title.add(jLabel);
-
-        JPanel stundent_teacher = new JPanel();
-        stundent_teacher.setLayout(new BoxLayout(stundent_teacher,BoxLayout.X_AXIS));
-
-        JPanel jPanel1 = new JPanel();
-        jPanel1.setLayout(new BoxLayout(jPanel1, BoxLayout.Y_AXIS));
-
-        JLabel jLabel1 = new JLabel("Nome");
-
-        JTextField student_name = new JTextField();
-        student_name.setSize(new Dimension(500,80));
-        student_name.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-        jPanel1.add(jLabel1);
-        jPanel1.add(student_name);
-
-        JPanel jPanel2 = new JPanel();
-        jPanel2.setLayout(new BoxLayout(jPanel2, BoxLayout.Y_AXIS));
-
-        JLabel jLabel2 = new JLabel("Professor");
-
-        JTextField teacher_name = new JTextField();
-        teacher_name.setSize(new Dimension(500,80));
-        teacher_name.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-        jPanel2.add(jLabel2);
-        jPanel2.add(teacher_name);
-
-
-        JPanel jPanel3 = new JPanel();
-
-        stundent_teacher.add(jPanel1);
-        stundent_teacher.add(jPanel3);
-        stundent_teacher.add(jPanel2);
-
-        stundent_teacher.setMaximumSize(new Dimension(1000,600));
-
-        topView.add(title);
-        topView.add(stundent_teacher);
-
+        jPanel.setSize(900,900);
+        jPanel.setPreferredSize(new Dimension(1500,900));
 
         JPanel body = new JPanel();
-        JPanel flooter = new JPanel();
+        JPanel footer = new JPanel();
 
-        jPanel.add(topView);
+        jPanel.add(ModalViewDefense.topView());
         jPanel.add(body);
-        jPanel.add(flooter);
+        jPanel.add(footer);
 
         jFrame.add(jPanel);
 
 
+
+    }
+
+    private static JPanel topView(){
+
+        Student student =  new Student();
+        student.setName("Natanael Bezerra");
+
+        Teacher teacher = new Teacher();
+        teacher.setName("Edleno Moura");
+
+
+        JPanel topView = new JPanel();
+        topView.setLayout(new BoxLayout(topView,BoxLayout.Y_AXIS));
+        topView.setPreferredSize(new Dimension(1500,400));
+        topView.setBackground(Color.decode("#d7d414"));
+
+        JLabel jLabel = new JLabel("Informações");
+        JPanel title = new JPanel();
+        title.setMaximumSize(new Dimension(1920, 50));
+        title.add(jLabel);
+
+        JPanel student_teacher = new JPanel();
+        student_teacher.setLayout(new FlowLayout());
+        student_teacher.setPreferredSize(new Dimension(500,80));
+
+        JPanel jPanel3 = new JPanel();
+        jPanel3.setMaximumSize(new Dimension(100,1080));
+
+        student_teacher.add(ModalViewDefense.createTopContainer("Aluno",student ));
+        student_teacher.add(jPanel3);
+        student_teacher.add(ModalViewDefense.createTopContainer("Professor",teacher ));
+
+        topView.setBackground(Color.decode("#1c8c17"));
+
+
+        topView.add(title);
+        topView.add(student_teacher);
+
+        return topView;
+
+    }
+
+    private static JPanel createTopContainer(String title, Person person){
+
+        JPanel jPanel5 = new JPanel();
+        jPanel5.setLayout(new BoxLayout(jPanel5,BoxLayout.Y_AXIS));
+
+
+        JPanel infoSuperior = new JPanel();
+        infoSuperior.setLayout(new FlowLayout());
+        infoSuperior.setMaximumSize(new Dimension(1500,50));
+
+        JPanel infoInferior = new JPanel();
+
+        infoInferior.setMaximumSize(new Dimension(1500,150));
+
+
+        JLabel jLabel1 = new JLabel(title);
+        infoSuperior.add(jLabel1);
+
+        JTextField student_name = new JTextField();
+        student_name.setFocusable(false);
+        student_name.setText(person.getName());
+        student_name.setFont(new Font("Ubuntu", Font.BOLD, 18));
+        student_name.setEditable(false);
+        student_name.setBackground( Color.decode("#ffffff"));
+        student_name.setSize(new Dimension(500,80));
+        student_name.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        infoInferior.add(student_name);
+
+        JPanel harf = new JPanel();
+        infoInferior.add(harf);
+
+        JButton jButton = new JButton("Edit");
+        jButton.setSize(new Dimension(80,80));
+        infoInferior.add(jButton);
+
+        jPanel5.add(infoSuperior);
+        jPanel5.add(infoInferior);
+
+        return jPanel5;
 
     }
     @Override
